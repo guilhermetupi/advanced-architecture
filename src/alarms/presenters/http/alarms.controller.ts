58552a740166658126aaf4258449f8dfa2675689
Guilherme Tupinamba <guilhermetupinamba@gmail.com>
2024-02-13 01:13:31 -0300

    refactor: remove unecessary AlarmsController methods

--- src/alarms/presenters/http/alarms.controller.ts
+++ src/alarms/presenters/http/alarms.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AlarmsService } from '../../application/alarms.service';
 import { CreateAlarmDto } from './dto/create-alarm.dto';
-import { UpdateAlarmDto } from './dto/update-alarm.dto';
 
 @Controller('alarms')
 export class AlarmsController {
@@ -23,20 +14,5 @@ export class AlarmsController {
   @Get()
   findAll() {
     return this.alarmsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.alarmsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAlarmDto: UpdateAlarmDto) {
-    return this.alarmsService.update(+id, updateAlarmDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.alarmsService.remove(+id);
   }
 }
