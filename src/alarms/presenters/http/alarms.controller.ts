dfba226b271f3ecc45d412f1652b257829d3365f
Guilherme Tupinamba <guilhermetupinamba@gmail.com>
2024-02-13 01:19:57 -0300

    refactor: call AlarmsService with CreateAlarmCommand on AlarmsController

--- src/alarms/presenters/http/alarms.controller.ts
+++ src/alarms/presenters/http/alarms.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AlarmsService } from '../../application/alarms.service';
 import { CreateAlarmDto } from './dto/create-alarm.dto';
+import { CreateAlarmCommand } from 'src/alarms/application/commands/create-alarm.command';
 
 @Controller('alarms')
 export class AlarmsController {
   constructor(private readonly alarmsService: AlarmsService) {}
 
   @Post()
-  create(@Body() createAlarmDto: CreateAlarmDto) {
-    return this.alarmsService.create(createAlarmDto);
+  create(@Body() { name, severity }: CreateAlarmDto) {
+    return this.alarmsService.create(new CreateAlarmCommand(name, severity));
   }
 
   @Get()
