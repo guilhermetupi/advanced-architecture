898ad5828938e1d9880bd672b45988e1f8ecbec8
Guilherme Tupinamba <guilhermetupinamba@gmail.com>
2024-02-13 01:14:00 -0300

    refactor: remove uncessary AlarmsService methods

--- src/alarms/application/alarms.service.ts
+++ src/alarms/application/alarms.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAlarmDto } from '../presenters/http/dto/create-alarm.dto';
-import { UpdateAlarmDto } from '../presenters/http/dto/update-alarm.dto';
 
 @Injectable()
 export class AlarmsService {
@@ -10,17 +9,5 @@ export class AlarmsService {
 
   findAll() {
     return `This action returns all alarms`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} alarm`;
-  }
-
-  update(id: number, updateAlarmDto: UpdateAlarmDto) {
-    return `This action updates a #${id} alarm`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} alarm`;
   }
 }
