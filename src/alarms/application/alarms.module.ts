7fe047398883325843c3d4fb2e11f1cd5eddce3e
Guilherme Tupinamba <guilhermetupinamba@gmail.com>
2024-02-13 01:02:52 -0300

    refactor: move alarms service and module to application folder

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AlarmsService } from './alarms.service';
-import { AlarmsController } from './alarms.controller';
+import { AlarmsController } from '../alarms.controller';
 
 @Module({
   controllers: [AlarmsController],
